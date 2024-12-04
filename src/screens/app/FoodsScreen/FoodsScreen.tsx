fa24c394817e032934f8823de86e8842beb400b0
rafael-mingossi <rafaelmingossi@gmail.com>
2024-12-05 08:08:23 +1100

    removing package-lock json

--- src/screens/app/FoodsScreen/FoodsScreen.tsx
+++ src/screens/app/FoodsScreen/FoodsScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Button} from 'react-native';
+import {View} from 'react-native';
 
 import {CustomTabMenu, ScreenFixedHeader} from '@components';
 

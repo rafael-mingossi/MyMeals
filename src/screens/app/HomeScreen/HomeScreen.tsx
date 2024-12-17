b01ff33709d88d12cf7bb56cd6764ede52f25469
rafael-mingossi <rafaelmingossi@gmail.com>
2024-12-17 22:30:50 +1100

    get meal by user and date added to HomeScreen initial only

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {useGetMealsByUserAndDate} from '@domain';
+import {useAuthCredentials, useCalendar} from '@services';
 import {SheetManager} from 'react-native-actions-sheet';
 
 import {
@@ -8,10 +10,29 @@ import {
   CalendarWidget,
   ButtonFloat,
   Icon,
+  Box,
+  ActivityIndicator,
+  Text,
 } from '@components';
 import {AppTabScreenProps} from '@routes';
 
 export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
+  const {authCredentials} = useAuthCredentials();
+  const {dateSelected} = useCalendar();
+
+  const {meals, isLoading} = useGetMealsByUserAndDate(
+    authCredentials?.user.id!,
+    dateSelected.dateString,
+  );
+
+  if (isLoading) {
+    return (
+      <Box flex={1} justifyContent="center" alignItems="center">
+        <ActivityIndicator />
+      </Box>
+    );
+  }
+
   function openMenu() {
     SheetManager.show('bs-menu');
   }
@@ -24,6 +45,9 @@ export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
         component: <CalendarWidget />,
       }}>
       <CalendarModal />
+      {meals.map(meal => (
+        <Text key={meal.id}>Total Calories: {meal.totalCalories}</Text>
+      ))}
       <ButtonFloat onPress={openMenu}>
         <Icon name={'plus'} size={30} color={'white'} />
       </ButtonFloat>
